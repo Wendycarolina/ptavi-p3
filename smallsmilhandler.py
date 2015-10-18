3d645ca0fb067545f3d5f9992d54f74a32803f7c
Wendy Carolina Iza <carolina.iza.g@gmail.com>
2015-10-18 21:44:41 +0200

    smallsmil pep8

--- smallsmilhandler.py
+++ smallsmilhandler.py
@@ -3,33 +3,58 @@
 
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
+import smallsmilhandler
+import sys
+import json
+from urllib.request import urlretrieve
 
-class SmallSMILHandler(ContentHandler):
-    
-    def __init__ (self):
-        self.etiqueta = ['root-layout', 'region', 'img', 'audio', 'textstream']
-        self.atributo = {'root-layout': ['width', 'height', 'background-color'],
-                         'region': ['id', 'top', 'bottom', 'left', 'right'],
-                         'img': ['src', 'region', 'begin', 'dur'],
-                         'audio':['src', 'begin', 'dur'],
-                         'textstream': ['src', 'region']}
-        self.lista = []
 
-    def startElement(self, name, attrs):
-        dicc = {}
-        if name in self.etiqueta:
-            dicc['name'] = name
-            for i in self.atributo[name]:
-                dicc[i] = attrs.get(i, "")
-            self.lista.append(dicc)
+class KaraokeLocal():
+    def __init__(self, fich):
+        parser = make_parser()
+        cHandler = smallsmilhandler.SmallSMILHandler()
+        parser.setContentHandler(cHandler)
+        parser.parse(fich)
+        self.lista = cHandler.get_tags()
 
-    def get_tags(self):
-        return self.lista
+    def __str__(self):
+        elemento = ''
+        for dicc in self.lista:
+            elemento += dicc['name']
+            for i in dicc:
+                if dicc[i] and i != 'name':
+                    elemento += "\t" + i + '="' + dicc[i] + '"'
+            elemento += '\n'
+        return elemento
+
+    def to_json(self, fich):
+        if fich != 'karaoke.json':
+            archivo = open('karaoke.json', 'w')
+            contenido = json.dumps(self.lista)
+            archivo.write(contenido)
+
+    def do_local(self):
+        for dicc in self.lista:
+            for etiqueta in dicc:
+                if dicc[etiqueta].find('http://') == 0:
+                    url = dicc[etiqueta]
+                    elemento = url[url.rfind('/') + 1:]
+                    urlretrieve(url, elemento)
+                    modifica = dicc[etiqueta].split('/')[-1]
+                    dicc[etiqueta] = modifica
 
 
 if __name__ == "__main__":
-    parser = make_parser()
-    cHandler = SmallSMILHandler()
-    parser.setContentHandler(cHandler)
-    parser.parse(open('karaoke.smil'))
-    print(cHandler.get_tags())
+    try:
+        fich = open(sys.argv[1], 'r')
+    except IOError:
+        sys.exit("Usage: python karaoke.py file.smil")
+    except IndexError:
+        sys.exit("Usage: python karaoke.py file.smil")
+
+    karaoke = KaraokeLocal(fich)
+    print(karaoke)
+    karaoke.to_json(fich)
+    karaoke.do_local()
+    karaoke.to_json(fich)
+    print(karaoke)
